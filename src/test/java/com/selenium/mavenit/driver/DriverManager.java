9e236809b03bb0c0bbc35ba0eb56164019948db2
mr0075 <mr0075@email.com>
2020-02-22 13:29:35 +0000

    Selenium core changes

--- src/test/java/com/selenium/mavenit/driver/DriverManager.java
+++ src/test/java/com/selenium/mavenit/driver/DriverManager.java
@@ -1,24 +1,46 @@
 package com.selenium.mavenit.driver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
 public class DriverManager {
 
-    public void openBrowser(){
+
+    private static WebDriver driver;
+    private String browser = "";
+
+    public void openBrowser() {
+        switch (browser) {
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "ie":
+                WebDriverManager.iedriver().setup();
+                driver = new InternetExplorerDriver();
+                break;
+            default:
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+        }
+    }
+
+    public void maxBrowser() {
+        driver.manage().window().maximize();
+    }
+
+    public void applyImpcitWait() {
 
     }
 
-    public void maxBrowser(){
-
+    public void navigateTo(String url) {
+        driver.get(url);
     }
 
-    public void applyImpcitWait(){
-
-    }
-
-    public void navigateTo(String url){
-
-    }
-
-    public void closeBrowser(){
-
+    public void closeBrowser() {
+        driver.quit();
     }
 }
