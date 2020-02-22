3384206bed4e3d358d70444408558b5d2b284af7
mr0075 <mr0075@email.com>
2020-02-22 15:57:12 +0000

    page factory impl.

--- src/test/java/com/selenium/mavenit/driver/DriverManager.java
+++ src/test/java/com/selenium/mavenit/driver/DriverManager.java
@@ -5,10 +5,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.PageFactory;
 
 import java.util.concurrent.TimeUnit;
 
 public class DriverManager {
+
+
+    public DriverManager(){
+        PageFactory.initElements(driver,this);
+    }
 
 
     public static WebDriver driver;
